a0b90a7563d302c527da23ccc1a4fbe6e3b47f74
heldemildej <heldemildemuco@gmail.com>
2025-09-23 16:13:52 -0300

    feat(domain): adiciona entidades anêmicas Category e Product

--- CleanArchMvc.Domain/Entities/Category.cs
+++ CleanArchMvc.Domain/Entities/Category.cs
@@ -9,6 +9,9 @@ namespace CleanArchMvc.Domain.Entities
 {
     public class Category
     {
-  
+        public int Id { get; set; }
+        public string Name { get; set; }
+
+        public ICollection<Product> Products { get; set; }
     }
 }
