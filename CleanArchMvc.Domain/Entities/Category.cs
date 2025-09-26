92b4b4f127d590bbc033a02a59457d1ff0a0802d
heldemildej <heldemildemuco@gmail.com>
2025-09-26 01:05:15 -0300

    test(domain): adiciona testes unitários para Category

--- CleanArchMvc.Domain/Entities/Category.cs
+++ CleanArchMvc.Domain/Entities/Category.cs
@@ -22,7 +22,7 @@ namespace CleanArchMvc.Domain.Entities
         // Construtor para categoria existente.
         public Category(int id, string name)
         {
-            DomainExceptionValidation.When(id < 0, "Invalid Id value");
+            DomainExceptionValidation.When(id < 0, "Invalid Id value.");
             Id = id;
             ValidateDomain(name);
         }
@@ -37,7 +37,7 @@ namespace CleanArchMvc.Domain.Entities
         private void ValidateDomain(string name)
         {
             DomainExceptionValidation.When(string.IsNullOrEmpty(name),
-                "Invalid name. Name is requerid");
+                "Invalid name. Name is required");
 
             DomainExceptionValidation.When(name.Length < 3,
                 "Invalid name too short, minimum 3 characteres");
