c2270f1a7b2642ecba85651446719b253e4372c2
Heldemilde João <heldemildemuco@gmail.com>
2026-03-29 17:08:21 +0100

    Fix: sincronização do banco de dados EF Core e configuração da conexão

---
+++ CleanArchMvc.Infra.Data/Migrations/20260329153312_InitialCreate.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace CleanArchMvc.Infra.Data.Migrations
+{
+    public partial class InitialCreate : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
