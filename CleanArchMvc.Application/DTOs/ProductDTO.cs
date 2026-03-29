039c60327be0014ded82b01b517800f2609cb879
Heldemilde João <heldemildemuco@gmail.com>
2026-03-29 20:26:47 +0100

    feat(application): adiciona DataAnnotations com DisplayName, MaxLength, DisplayFormat e DataType no ProductDTO

--- CleanArchMvc.Application/DTOs/ProductDTO.cs
+++ CleanArchMvc.Application/DTOs/ProductDTO.cs
@@ -1,6 +1,9 @@
 ﻿using CleanArchMvc.Domain.Entities;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,14 +13,37 @@ namespace CleanArchMvc.Application.DTOs
     public class ProductDTO
     {
         public int Id { get; set; }
+
+        [Required(ErrorMessage = "The Name is required")]
+        [MinLength(3)]
+        [MaxLength(100)]
+        [DisplayName("Name")]
         public string Name { get; set; }
+
+        [Required(ErrorMessage = "The Description is required")]
+        [MinLength(5)]
+        [MaxLength(100)]
         public string Description { get; set; }
+
+        [Required(ErrorMessage = "Price is required")]
+        [Column(TypeName = "decimal(18,2)")]
+        [DisplayFormat(DataFormatString = "{0:C2}")]
+        [DataType(DataType.Currency)]
+        [DisplayName("Price")]
         public decimal Price { get; set; }
+
+        [Required(ErrorMessage = "The Stock is required")]
+        [Range(1, 9999)]
+        [DisplayName("Stock")]
         public int Stock { get; set; }
+
+        [MaxLength(250)]
+        [DisplayName("Product Image")]
         public string Img { get; set; }
 
-        // Relacionamento com categoria
+        // Relacionamento com Category
+        [DisplayName("Categories")]
+        public int CategoryId { get; set; }
         public Category Category { get; set; }
-        public int CategoryId { get; set; } 
     }
 }
