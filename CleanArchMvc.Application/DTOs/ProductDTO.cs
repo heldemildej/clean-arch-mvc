6e362a337f01c43f678e6c9bf0feab0a1204611a
Heldemilde João <heldemildemuco@gmail.com>
2026-03-29 18:55:41 +0100

    feat(application): adiciona DTOs de Category e Product

---
+++ CleanArchMvc.Application/DTOs/ProductDTO.cs
@@ -0,0 +1,23 @@
+﻿using CleanArchMvc.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CleanArchMvc.Application.DTOs
+{
+    public class ProductDTO
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public decimal Price { get; set; }
+        public int Stock { get; set; }
+        public string Img { get; set; }
+
+        // Relacionamento com categoria
+        public Category Category { get; set; }
+        public int CategoryId { get; set; } 
+    }
+}
