467c462e797760831a8c028dfcd5d8e2a384965a
Heldemilde João <heldemildemuco@gmail.com>
2026-04-10 00:33:39 +0100

    feat(api): adiciona controllers de Products e Categories e remove arquivos padrão

---
+++ CleanArchMvc.API/Controllers/CategoriesController.cs
@@ -0,0 +1,79 @@
+﻿using CleanArchMvc.Application.DTOs;
+using CleanArchMvc.Application.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Threading.Tasks;
+
+namespace CleanArchMvc.API.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class CategoriesController : ControllerBase
+    {
+        private readonly ICategoryService _categoryService;
+
+        public CategoriesController(ICategoryService categoryService)
+        {
+            _categoryService = categoryService;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Get()
+        {
+            var result = await _categoryService.GetCategoriesAsync();
+            return Ok(result);
+        }
+
+        [HttpGet("{id}")]
+        public async Task<IActionResult> Get(int id)
+        {
+            var result = await _categoryService.GetByIdAsync(id);
+
+            if (result == null)
+                return NotFound();
+
+            return Ok(result);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Post([FromBody] CategoryDTO category)
+        {
+            if (category == null)
+                return BadRequest();
+
+            await _categoryService.AddAsync(category);
+            return CreatedAtAction(nameof(Get), new { id = category.Id }, category);
+        }
+
+        [HttpPut]
+        public async Task<IActionResult> Put([FromBody] CategoryDTO category)
+        {
+            if (category == null)
+                return BadRequest();
+
+            try
+            {
+                await _categoryService.UpdateAsync(category);
+                return Ok(category);
+            }
+            catch (ApplicationException ex)
+            {
+                return NotFound(ex.Message);
+            }
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> Delete(int id)
+        {
+            try
+            {
+                await _categoryService.RemoveAsync(id);
+                return NoContent();
+            }
+            catch (ApplicationException ex)
+            {
+                return NotFound(ex.Message);
+            }
+        }
+    }
+}
---
+++ CleanArchMvc.API/Controllers/ProductController.cs
@@ -0,0 +1,81 @@
+﻿using CleanArchMvc.Application.Products.Commands;
+using CleanArchMvc.Application.Products.Queries;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Threading.Tasks;
+
+namespace CleanArchMvc.API.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class ProductsController : ControllerBase
+    {
+        private readonly IMediator _mediator;
+
+        public ProductsController(IMediator mediator)
+        {
+            _mediator = mediator;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Get()
+        {
+            var result = await _mediator.Send(new GetProductsQuery());
+            return Ok(result);
+        }
+
+        [HttpGet("{id}")]
+        public async Task<IActionResult> Get(int id)
+        {
+            var result = await _mediator.Send(new GetProductByIdQuery(id));
+
+            if (result == null)
+                return NotFound();
+
+            return Ok(result);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Post([FromBody] ProductCreateCommand command)
+        {
+            if (command == null)
+                return BadRequest();
+
+            var result = await _mediator.Send(command);
+
+            return CreatedAtAction(nameof(Get), new { id = result.Id }, result);
+        }
+
+        [HttpPut]
+        public async Task<IActionResult> Put([FromBody] ProductUpdateCommand command)
+        {
+            if (command == null)
+                return BadRequest();
+
+            try
+            {
+                var result = await _mediator.Send(command);
+                return Ok(result);
+            }
+            catch (ApplicationException ex)
+            {
+                return NotFound(ex.Message);
+            }
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> Delete(int id)
+        {
+            try
+            {
+                await _mediator.Send(new ProductRemoveCommand(id));
+                return NoContent();
+            }
+            catch (ApplicationException ex)
+            {
+                return NotFound(ex.Message);
+            }
+        }
+    }
+}
