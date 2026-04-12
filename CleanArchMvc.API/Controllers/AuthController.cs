b791d1266e94b40aed75b3f10197fb41f06939a7
Heldemilde João <heldemildemuco@gmail.com>
2026-04-12 01:38:59 +0100

    feat(auth): cria AuthController para geração de token JWT

---
+++ CleanArchMvc.API/Controllers/AuthController.cs
@@ -0,0 +1,28 @@
+﻿using CleanArchMvc.Infra.IoC.Security;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CleanArchMvc.API.Controllers
+{
+   
+    [ApiController]
+    [AllowAnonymous]
+    [Route("api/auth")]
+    public class AuthController : ControllerBase
+    {
+        private readonly TokenService _tokenService;
+
+        public AuthController(TokenService tokenService)
+        {
+            _tokenService = tokenService;
+        }
+
+        [AllowAnonymous]
+        [HttpGet("token")]
+        public IActionResult GetToken()
+        {
+            var token = _tokenService.GenerateToken();
+            return Ok(new { token });
+        }
+    }
+}
