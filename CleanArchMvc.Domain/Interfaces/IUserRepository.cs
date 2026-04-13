b5fd59107dfaab3e45dff0457a36680e064fc03f
Heldemilde João <heldemildemuco@gmail.com>
2026-04-13 02:05:52 +0100

    feat(user): criar entidade User e repository para autenticação

---
+++ CleanArchMvc.Domain/Interfaces/IUserRepository.cs
@@ -0,0 +1,11 @@
+﻿using CleanArchMvc.Domain.Entities;
+using System.Threading.Tasks;
+
+namespace CleanArchMvc.Domain.Interfaces
+{
+    public interface IUserRepository
+    {
+        Task<User> GetByUsernameAsync(string username);
+        Task AddAsync(User user);
+    }
+}
---
+++ CleanArchMvc.Infra.Data/Repositories/UserRepository.cs
@@ -0,0 +1,29 @@
+﻿using CleanArchMvc.Domain.Entities;
+using CleanArchMvc.Domain.Interfaces;
+using CleanArchMvc.Infra.Data.Context;
+using Microsoft.EntityFrameworkCore;
+using System.Threading.Tasks;
+
+namespace CleanArchMvc.Infra.Data.Repositories
+{
+    public class UserRepository : IUserRepository
+    {
+        private readonly ApplicationDbContext _context;
+
+        public UserRepository(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<User> GetByUsernameAsync(string username)
+        {
+            return await _context.Users.FirstOrDefaultAsync(x => x.Username == username);
+        }
+
+        public async Task AddAsync(User user)
+        {
+            _context.Users.Add(user);
+            await _context.SaveChangesAsync();
+        }
+    }
+}
